49b140a835640034d89c9043e235e7ccba8239e5
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2021-12-05 07:36:59 +0300

    Create isim_soyisim_grublama_zip.py

---
+++ map-filter-reduce-zip Fonksiyonları/isim_soyisim_grublama_zip.py
@@ -0,0 +1,4 @@
+isimler=["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
+soyisimler=["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
+for i,k in zip(isimler,soyisimler):
+    print("İsim: {} Soyisim: {}".format(i,k))
